78585e0b2ce44a2b8ca9c7fc1123542c1d38c163
Evelyn Paplauskas <evelyn.paplauskas@outlook.com>
2020-07-02 09:53:48 +1000

    Add task to list

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
-import { Input } from 'semantic-ui-react';
+import React, { useState, useContext } from "react";
+import { Input } from "semantic-ui-react";
+import TodoContext from "./../contexts/TodoContext";
+
 
 const TodoInput = () => {
-  const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState("");
+  const { addTodo } = useContext(TodoContext);
 
   const handleInput = (event) => {
     setInputText(event.target.value);
-  };
 
+    if (event.keyCode === 13) {
+      addTodo(inputText);
+      setInputText('');
+    }
+  };
+  
   return <Input placeholder="Add To-Do" onKeyUp={handleInput} />;
 };
 
