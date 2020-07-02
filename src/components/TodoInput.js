9813779b5ede9e8bd207bd5ee003255d1495ceda
Ross Baker <rossbaker32@gmail.com>
2020-07-02 12:42:19 +1000

    challenge solution

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 import { Input } from 'semantic-ui-react';
+import TodoContext from '../contexts/TodoContext';
+import { v4 as uuidv4 } from 'uuid';
+
 
 const TodoInput = () => {
   const [inputText, setInputText] = useState('');
 
-  const handleInput = (event) => {
-    setInputText(event.target.value);
+  const handleInput = (event, context) => {
+    if (event.which === 13 && inputText != '') {
+      context.setTodoTasks([...context.todoTasks, {id: uuidv4(), text: event.target.value}]);
+      event.target.value = '';
+    } else {
+      setInputText(event.target.value);
+    }
   };
 
-  return <Input placeholder="Add To-Do" onKeyUp={handleInput} />;
+  return (
+    <TodoContext.Consumer >
+      {context => (
+        <Input placeholder="Add To-Do" onKeyUp={(event) => handleInput(event, context)} />
+      )}
+    </TodoContext.Consumer>
+  )
 };
 
 export default TodoInput;
